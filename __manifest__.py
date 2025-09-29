c4f0a4066c89f2a03d6a211a796d33e397fc4ea8
Baruch <tu_correo@ejemplo.com>
2025-09-28 22:02:29 -0600

    Migración a Odoo 17: Actualización completa de módulo de gestión de servicios
    - Actualizado __manifest__.py para compatibilidad con Odoo 17
    - Migrado calendar_views.js a ES6/OWL con nuevas APIs
    - Actualizado vistas XML de calendario con plantillas OWL
    - Mejorada integración con base_geolocalize y web_map
    - Optimizada estructura de plantillas QWeb
    - Añadida funcionalidad de mapas y geolocalización
    - Actualizados estilos CSS para nueva interfaz
    - Mantenida compatibilidad con funcionalidades existentes

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,21 @@
     'author': 'INMOSER84',
     'website': 'https://github.com/INMOSER84/inmoser_service_order',
     'license': 'LGPL-3',
-    'depends': ['base', 'web', 'mail', 'account', 'stock', 'hr', 'portal', 'calendar', 'product', 'contacts'],
+    'depends': [
+        'base', 
+        'web', 
+        'mail', 
+        'account', 
+        'stock', 
+        'hr', 
+        'portal', 
+        'calendar', 
+        'product', 
+        'contacts',
+        'base_geolocalize',  # Añadido para funcionalidad de mapas
+        'web_map',           # Añadido para vistas de mapa
+        'sale',              # Añadido para integración con ventas
+    ],
     'data': [
         'security/ir.model.access.csv',
         'security/inmoser_security.xml',
@@ -19,6 +33,7 @@
         'views/service_order_views.xml',
         'views/service_order_actions.xml',
         'views/service_order_calendar.xml',
+        'views/service_order_map.xml',  # Añadido para vista de mapa
         'views/service_equipment_views.xml',
         'views/service_type_views.xml',
         'views/hr_employee_views.xml',
@@ -38,6 +53,7 @@
             'modulo/static/src/css/service_order.scss',
             'modulo/static/src/js/calendar_views.js',
             'modulo/static/src/js/service_order_qr.js',
+            'modulo/static/src/js/service_order_map.js',  # Añadido para funcionalidad de mapa
         ],
         'web.assets_frontend': [
             'modulo/static/src/css/portal.scss',
@@ -45,6 +61,7 @@
         'web.assets_qweb': [
             'modulo/static/src/xml/service_order_qr.xml',
             'modulo/static/src/xml/calendar_templates.xml',
+            'modulo/static/src/xml/map_templates.xml',  # Añadido para plantillas de mapa
         ],
     },
     'demo': ['demo/demo_data.xml'],
@@ -60,7 +77,10 @@
     'installable': True,
     'application': True,
     'auto_install': False,
-    'external_dependencies': {'python': ['qrcode']},
+    'external_dependencies': {
+        'python': ['qrcode', 'requests'],  # Añadido requests para API de mapas
+        'bin': ['libgeos-dev'],  # Añadido para funcionalidad geográfica
+    },
     'development_status': 'Beta',
     'price': 0,
     'currency': 'EUR',
@@ -77,6 +97,7 @@ Features:
     * Integration with accounting, inventory and HR modules
     * Comprehensive reporting system
     * Mobile-friendly interface
+    * Map views for service location tracking (NEW in v17)
 
 This module is ideal for companies that provide technical services such as:
     * HVAC maintenance
--- static/src/js/calendar_views.js
+++ static/src/js/calendar_views.js
@@ -1,146 +1,154 @@
-odoo.define('inmoser_service_order.calendar_views', function (require) {
-    "use strict";
+/** @odoo-module **/
 
-    var CalendarView = require('web.CalendarView');
-    var CalendarRenderer = require('web.CalendarRenderer');
-    var CalendarModel = require('web.CalendarModel');
-    var viewRegistry = require('web.view_registry');
-    var core = require('web.core');
-    var _t = core._t;
+import { CalendarView } from "@web/views/calendar/calendar_view";
+import { CalendarRenderer } from "@web/views/calendar/calendar_renderer";
+import { CalendarModel } from "@web/views/calendar/calendar_model";
+import { viewRegistry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
+import { Component } from "@odoo/owl";
 
-    var ServiceOrderCalendarModel = CalendarModel.extend({
-        /**
-         * Sobrescribir el método load para filtrar órdenes de servicio
-         * por estado y técnico asignado
-         */
-        load: function (params) {
-            var self = this;
-            params.domain = params.domain || [];
-            
-            // Filtrar por estados relevantes
-            params.domain.push(['state', 'in', ['pending', 'in_progress', 'scheduled']]);
-            
-            return this._super(params);
-        },
+export class ServiceOrderCalendarModel extends CalendarModel {
+    async load(params) {
+        params.domain = params.domain || [];
+        
+        // Filtrar por estados relevantes
+        params.domain.push(['state', 'in', ['pending', 'in_progress', 'scheduled']]);
+        
+        const result = await super.load(params);
+        return result;
+    }
 
-        /**
-         * Obtener información adicional para mostrar en el calendario
-         */
-        getCalendarEventRecord: function (record) {
-            var event = this._super(record);
-            
-            // Agregar información del cliente
-            if (record.partner_id) {
-                event.partner_name = record.partner_id[1];
-            }
-            
-            // Agregar información del técnico
-            if (record.technician_id) {
-                event.technician_name = record.technician_id[1];
-            }
-            
-            // Agregar tipo de servicio
-            if (record.service_type_id) {
-                event.service_type = record.service_type_id[1];
-            }
-            
-            return event;
-        },
-    });
+    getCalendarEventRecord(record) {
+        const event = super.getCalendarEventRecord(record);
+        
+        // Agregar información del cliente
+        if (record.partner_id) {
+            event.partner_name = record.partner_id[1];
+        }
+        
+        // Agregar información del técnico
+        if (record.technician_id) {
+            event.technician_name = record.technician_id[1];
+        }
+        
+        // Agregar tipo de servicio
+        if (record.service_type_id) {
+            event.service_type = record.service_type_id[1];
+        }
+        
+        return event;
+    }
+}
 
-    var ServiceOrderCalendarRenderer = CalendarRenderer.extend({
-        /**
-         * Personalizar la visualización de eventos en el calendario
-         */
-        render_event: function (event) {
-            var $event = this._super(event);
-            
-            // Agregar información adicional al evento
-            var title = event.title;
-            if (event.partner_name) {
-                title += ' - ' + event.partner_name;
-            }
-            if (event.technician_name) {
-                title += ' (' + event.technician_name + ')';
-            }
-            
-            $event.find('.fc-title').text(title);
-            
-            // Agregar clase según el estado para diferentes colores
-            if (event.state === 'pending') {
-                $event.addClass('o_event_pending');
-            } else if (event.state === 'in_progress') {
-                $event.addClass('o_event_in_progress');
-            } else if (event.state === 'scheduled') {
-                $event.addClass('o_event_scheduled');
-            }
-            
-            return $event;
-        },
+export class ServiceOrderCalendarRenderer extends CalendarRenderer {
+    setup() {
+        super.setup();
+        this.orm = useService("orm");
+        this.notification = useService("notification");
+    }
 
-        /**
-         * Personalizar el tooltip del evento
-         */
-        getEventTooltip: function (event) {
-            var tooltip = '<div class="o_calendar_tooltip">';
-            tooltip += '<strong>' + event.title + '</strong><br/>';
-            
-            if (event.partner_name) {
-                tooltip += _t('Customer: ') + event.partner_name + '<br/>';
-            }
-            
-            if (event.technician_name) {
-                tooltip += _t('Technician: ') + event.technician_name + '<br/>';
-            }
-            
-            if (event.service_type) {
-                tooltip += _t('Service Type: ') + event.service_type + '<br/>';
-            }
-            
-            tooltip += _t('Status: ') + event.state + '<br/>';
-            tooltip += '</div>';
-            
-            return tooltip;
-        },
-    });
+    getEventClass(event) {
+        let classes = super.getEventClass(event);
+        
+        // Agregar clase según el estado para diferentes colores
+        if (event.state === 'pending') {
+            classes += ' o_event_pending';
+        } else if (event.state === 'in_progress') {
+            classes += ' o_event_in_progress';
+        } else if (event.state === 'scheduled') {
+            classes += ' o_event_scheduled';
+        }
+        
+        return classes;
+    }
 
-    var ServiceOrderCalendarView = CalendarView.extend({
-        config: _.extend({}, CalendarView.prototype.config, {
-            Model: ServiceOrderCalendarModel,
-            Renderer: ServiceOrderCalendarRenderer,
-        }),
-        display_name: _t('Service Order Calendar'),
-        icon: 'fa-calendar',
-        calendarView: 'week',
-        init: function (viewInfo, params) {
-            this._super.apply(this, arguments);
-            
-            // Configurar campos del calendario
-            this.arch.attrs = _.extend({}, this.arch.attrs, {
-                date_start: 'scheduled_date',
-                date_stop: 'scheduled_date_end',
-                color: 'technician_id',
-                filters: [
-                    {
-                        field: 'technician_id',
-                        name: _t('Technician'),
-                    },
-                    {
-                        field: 'state',
-                        name: _t('State'),
-                    },
-                ],
-                mode: 'week',
-                quick_create: false,
-            });
-        },
-    });
+    getEventContent(event) {
+        let content = super.getEventContent(event);
+        
+        // Agregar información adicional al evento
+        let title = event.title;
+        if (event.partner_name) {
+            title += ' - ' + event.partner_name;
+        }
+        if (event.technician_name) {
+            title += ' (' + event.technician_name + ')';
+        }
+        
+        content.title = title;
+        return content;
+    }
 
-    viewRegistry.add('service_order_calendar', ServiceOrderCalendarView);
+    getEventTooltip(event) {
+        let tooltip = '<div class="o_calendar_tooltip">';
+        tooltip += '<strong>' + event.title + '</strong><br/>';
+        
+        if (event.partner_name) {
+            tooltip += _t('Customer: ') + event.partner_name + '<br/>';
+        }
+        
+        if (event.technician_name) {
+            tooltip += _t('Technician: ') + event.technician_name + '<br/>';
+        }
+        
+        if (event.service_type) {
+            tooltip += _t('Service Type: ') + event.service_type + '<br/>';
+        }
+        
+        tooltip += _t('Status: ') + event.state + '<br/>';
+        tooltip += '</div>';
+        
+        return tooltip;
+    }
 
-    return {
-        ServiceOrderCalendarModel: ServiceOrderCalendarModel,
-        ServiceOrderCalendarRenderer: ServiceOrderCalendarRenderer,
-        ServiceOrderCalendarView: ServiceOrderCalendarView,
-    };
-});
+    async onEventClicked(event) {
+        const { id } = event;
+        const action = {
+            type: "ir.actions.act_window",
+            res_model: "service.order",
+            res_id: id,
+            views: [[false, "form"]],
+            target: "current",
+        };
+        this.actionService.doAction(action);
+    }
+}
+
+export class ServiceOrderCalendarView extends CalendarView {
+    setup() {
+        super.setup();
+        this.renderer = ServiceOrderCalendarRenderer;
+        this.model = ServiceOrderCalendarModel;
+    }
+
+    get viewParams() {
+        const params = super.viewParams;
+        
+        // Configurar campos del calendario
+        return {
+            ...params,
+            dateStart: 'scheduled_date',
+            dateStop: 'scheduled_date_end',
+            color: 'technician_id',
+            filters: [
+                {
+                    field: 'technician_id',
+                    name: _t('Technician'),
+                },
+                {
+                    field: 'state',
+                    name: _t('State'),
+                },
+            ],
+            mode: 'week',
+            quickCreate: false,
+        };
+    }
+}
+
+ServiceOrderCalendarView.display_name = _t('Service Order Calendar');
+ServiceOrderCalendarView.icon = 'fa-calendar';
+ServiceOrderCalendarView.multiRecord = true;
+ServiceOrderCalendarView.searchMenuTypes = ['filter', 'groupBy'];
+
+viewRegistry.add('service_order_calendar', ServiceOrderCalendarView);
